28a5452f48b7a305f4a48f31016bba5daff3d20c
saishashank768 <142394996+saishashank768@users.noreply.github.com>
2024-01-24 23:59:53 +0530

    streamlitapp.py

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ from PIL import Image, ImageOps
 import numpy as np
 
 st.set_option('deprecation.showfileUploaderEncoding', False)
-
 @st.cache(allow_output_mutation=True)
 def load_model():
     model = tf.keras.models.load_model('/content/saved_models/my_model1.hdf5')
@@ -22,15 +21,18 @@ st.write("""
          )
 
 file = st.file_uploader("Please upload a crack image", type=["jpg", "png"])
-
+import cv2
+from PIL import Image, ImageOps
+import numpy as np
 def import_and_predict(image_data, model):
+    
     size = (180, 180)
     image = ImageOps.fit(image_data, size, Image.ANTIALIAS)
     img = np.asarray(image)
     img_reshape = img[np.newaxis, ...]
     prediction = model.predict(img_reshape)
+    
     return prediction
-
 if file is None:
     st.text("Please upload an image file")
 else:
@@ -38,6 +40,6 @@ else:
     st.image(image, use_column_width=True)
     predictions = import_and_predict(image, model)
     class_names = ['diagonal', 'horizontal', 'structural', 'vertical']
-    result = f"This image most likely is: {class_names[np.argmax(predictions)]}"
+    string = "This image most likely is: "+class_names[np.argmax(predictions)]"
     st.success(result)
 
